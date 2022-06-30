5939ad6436dbba0c31268467c0525037fcfa9903
Tanya Shl <t_shl@mail.ru>
2022-06-30 10:07:46 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ButtonExample, List } from './components/examples';
 
 function App() {
+  const onClick = () => {
+    console.log('click');
+  };
+
+  const users = [
+    { id: 0, name: 'Lucas' },
+    { id: 1, name: 'William' },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,10 +24,11 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
+        <ButtonExample text="Button example" onClick={onClick} />
+        <List list={users} />
       </header>
     </div>
   );
