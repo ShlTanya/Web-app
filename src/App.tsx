dcbf0c1cc02d675013a8022ed50dafbd02ca7aa1
Tanya Shl <t_shl@mail.ru>
2022-07-05 07:14:53 +0300

    HW3

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ButtonExample, List } from './components/examples';
-import { Title, ButtonMenuHamburger, Link } from './components';
+import {
+  Title,
+  ButtonMenuHamburger,
+  Link,
+  Button,
+  BtnTheme,
+  ButtonSearch,
+  ButtonUserName,
+  ButtonLike,
+} from './components/atom';
+import { ReactComponent as FavoriteIcon } from './assets/icon/FavoriteIcon.svg';
 
 function App() {
   let isMyOpen = true;
@@ -11,11 +20,6 @@ function App() {
     isMyOpen = !isMyOpen;
     console.log(isMyOpen);
   };
-
-  const users = [
-    { id: 0, name: 'Lucas' },
-    { id: 1, name: 'William' },
-  ];
 
   return (
     <div className="App">
@@ -33,9 +37,33 @@ function App() {
           Learn React
         </a>
         <Title text="Sign In" />
+        <Button btnTheme={BtnTheme.Primary} text="Primary" onClick={onClick} />
+        <Button btnTheme={BtnTheme.Secondary} text="Secondary" onClick={onClick} />
+        <Button btnTheme={BtnTheme.Secondary2} text="Secondary2" onClick={onClick} />
+        <Button
+          btnTheme={BtnTheme.WithIcon}
+          text="WithIcon"
+          icon={<FavoriteIcon />}
+          onClick={onClick}
+        />
+        <Button btnTheme={BtnTheme.Primary} text="Primary dis" disabled onClick={onClick} />
+        <Button btnTheme={BtnTheme.Secondary} text="Secondary  dis" disabled onClick={onClick} />
+        <Button btnTheme={BtnTheme.Secondary2} text="Secondary2  dis" disabled onClick={onClick} />
+        <Button
+          btnTheme={BtnTheme.WithIcon}
+          disabled
+          text="WithIcon"
+          icon={<FavoriteIcon />}
+          onClick={onClick}
+        />
+        <ButtonUserName isAuthorized={false} userName="" onClick={onClick} />
+        <ButtonUserName isAuthorized={true} userName="Tatsiana shl" onClick={onClick} />
         <ButtonMenuHamburger isOpen={isMyOpen} onClick={onClick} />
-        <ButtonExample text="Button example" onClick={onClick} />
-        <List list={users} />
+        <ButtonSearch onClick={onClick} />
+        <ButtonLike isLike disabled={false} onClick={onClick} />
+        <ButtonLike isLike disabled onClick={onClick} />
+        <ButtonLike isLike={false} disabled={false} onClick={onClick} />
+        <ButtonLike isLike={false} disabled onClick={onClick} />
       </header>
     </div>
   );
--- src/components/ButtonMenuHamburger/index.ts
+++
@@ -1,1 +0,0 @@
-export { ButtonMenuHamburger } from './ButtonMenuHamburger';
